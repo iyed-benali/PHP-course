a6a9627a7b678ac749abda6238be6abeed60e4b0
iyed-benali <aveyrokepler86@gmail.com>
2023-06-03 14:20:36 +0100

    arithmetic basics

---
+++ arithmetic.php
@@ -0,0 +1,29 @@
+<?php 
+//arithmetic opertors
+// + - ** / %
+$x = 10;
+$y= 2;
+$z = null;
+$z = $x +$y;
+$z = $x -$y;
+$z = $x **$y;
+$z = $x /$y;
+$z = $x %$y;
+// echo "{$z}";
+//Increment / decrement operators
+
+$counter = 0;
+$counter = $counter +1;
+$counter ++;
+$counter --;
+$counter +=2;
+echo "{$counter}";
+
+//operator precedence
+//  () 
+//  **   
+//  * / %
+//  + -   
+
+// il awlawiya ilkol wa7da mil operators hethom biltartib
+?>
