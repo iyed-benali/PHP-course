40773ae73e01eb3a925ae7112b3ca76419db01be
iyed-benali <aveyrokepler86@gmail.com>
2023-06-05 13:48:38 +0100

    switches

--- logicalOperators.php
+++ logicalOperators.php
@@ -1,5 +1,4 @@
  <?php
- 
 // && =true if both condition are true
 // || =true if atleast one condition is true
 // ! =true if the condition is false
---
+++ switches.php
@@ -0,0 +1,24 @@
+<?php
+
+$grade="A";
+switch($grade){
+    case("A"):
+        echo "  did great";
+        break;
+        case("B"):
+            echo "noice";
+            break;
+            case("C"):
+                echo "nahh";
+                break;
+                case("D"):
+                    echo "very bad ";
+                    break;
+                    case("F"):
+                        echo "reported";
+                        break;
+                        default:
+                        echo "hello";
+
+}
+?>
