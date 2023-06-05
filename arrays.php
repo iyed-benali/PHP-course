21ffea2877a8739f49e41fd4fa57821ee1d65a90
iyed-benali <aveyrokepler86@gmail.com>
2023-06-05 14:10:02 +0100

    arrays

---
+++ arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+$fruits=array("food1","food2","food3","food4");
+// echo $fruits[0];
+array_push($fruits,'food0');
+array_pop($fruits);
+array_shift($fruits);
+array_reverse($fruits);
+echo count($fruits);
+// foreach($fruits as $fruit){
+// echo $fruit;
+// }
+?>   
